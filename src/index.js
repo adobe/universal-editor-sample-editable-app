50c416b362f49175dcde6052836b21586d38e81d
vlad <ens16477@adobe.com>
2023-05-12 15:55:19 +0300

    fix: re-enable development app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@adobe/universal-editor-cors";
+import "@aem-sites/universal-editor-cors";
 import App from "./App";
 import "./index.css";
 
