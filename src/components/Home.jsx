867890e6d4becfb27591e1ca23387af390d9e459
Maximilian Voss <voss@adobe.com>
2023-05-31 16:13:17 +0200

    Feat: Using a XF to render About US

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,28 +17,28 @@ import "./Home.scss";
 /***
  * Displays a grid of current adventures
  */
- function Home() {
+function Home() {
     return (
-      <div className="Home">
-        <Teaser />
-        <Adventures />
-        <section className="newsletter">
-          <div className="content">
-            <Title itemID="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/title" itemProp="jcr:title" itemType="text"/>
-            <Text itemID="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/text" itemProp="text" itemType="richtext" />       
-          </div>
-          <button>Subscribe</button>
-        </section>
-        <section className="about-us">
-          <div className="content">
-            <Title itemID="urn:aemconnection:/content/wknd/us/en/about-us/jcr:content/root/container/title" itemProp="jcr:title" itemType="text"/>
-            <Container itemID="urn:aemconnection:/content/wknd/us/en/about-us/jcr:content/root/container/container" itemType="container" />       
-          </div>
-          <Link to={`/aboutus${window.location.search}`}>
-            <button className="dark">Read more</button>
-          </Link>
-        </section>
-    </div>
+        <div className="Home">
+            <Teaser />
+            <Adventures />
+            <section className="newsletter">
+                <div className="content">
+                    <Title itemID="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/title" itemProp="jcr:title" itemType="text"/>
+                    <Text itemID="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/text" itemProp="text" itemType="richtext" />
+                </div>
+                <button>Subscribe</button>
+            </section>
+            <section className="about-us">
+                <div className="content">
+                    <Title itemID="urn:aemconnection:/content/experience-fragments/wknd/language-masters/en/about-us/master/jcr:content/root/container/container/title_copy" itemProp="jcr:title" itemType="text"/>
+                    <Container itemID="urn:aemconnection:/content/experience-fragments/wknd/language-masters/en/about-us/master/jcr:content/root/container/container_339706497/container_copy" itemType="container" />
+                </div>
+                <Link to={`/aboutus${window.location.search}`}>
+                    <button className="dark">Read more</button>
+                </Link>
+            </section>
+        </div>
     );
 }
 
