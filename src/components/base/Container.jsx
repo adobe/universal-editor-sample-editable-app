564748e2fafcc71720d79cb5908850da427458f1
svinod <svinod@adobe.com>
2023-08-03 13:32:25 +0200

    fix: add support for image component

--- src/components/base/Container.jsx
+++ src/components/base/Container.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {fetchData} from '../../utils/fetchData';
 import Text from './Text';
 import Title from './Title';
+import Image from './Image';
+
 const Container = ({ itemID, itemType, isComponent = "" }) => {
   const [components, setComponents] = React.useState(null);
 
@@ -9,14 +11,34 @@ const Container = ({ itemID, itemType, isComponent = "" }) => {
     const components = [];
     for(let key in items) {
       const item = items[key];
-      const isContainer = !!item[":items"];
+      const type = item[":type"].split("/").pop();
+      let itemType, Component;
+      
+      switch(type) {
+        case "image": 
+          itemType = "media";
+          Component = Image;
+          break;
+        case "text": 
+          itemType = item.richText ? "richtext" : "text";
+          Component = item.type ? Title : Text;
+          break;
+        case "container": 
+          itemType = "container";
+          Component = Container;
+          break;
+        default: 
+          itemType = "component";
+          Component = () => (<div/>);
+          break;
+      }
+
       const props = {
         itemID: `${itemid}/${key}`,
-        itemType: isContainer ? "container" : item.richText ? "richtext" : "text",
+        itemType,
         data: item,
         isComponent: "component"
       };
-      const Component =  isContainer ? Container : item.type ? Title : Text;
       components.push(<Component key={key} {...props} />)
     }
     return components;
