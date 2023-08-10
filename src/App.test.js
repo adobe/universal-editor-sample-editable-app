48ec55192b94deb5d169f1b19c454e1107cec5f7
Benny Yih <benny.yih@perficient.com>
2023-08-10 16:51:24 -0600

    Ignore ".yarn" files / Fix stub App test / Move test & build dependencies to devDependencies in package.json Add Node 16 engine dependency due to ancient react-penpal wrapper for penpal version.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('WKND Sign in', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign in/i);
   expect(linkElement).toBeInTheDocument();
 });
