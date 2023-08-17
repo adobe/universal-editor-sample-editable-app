9f7a3f413123ddd394103269eaee9a97333d42a5
svinod <svinod@adobe.com>
2023-08-17 13:28:25 +0200

    fix: update model definition

--- src/components/base/Text.jsx
+++ src/components/base/Text.jsx
@@ -11,7 +11,8 @@ import {fetchData} from '../../utils/fetchData';
 
 const Text = (props) => {
   const {itemID, itemProp = "text", itemType, className, data: initialData, isComponent = false} = props;
-  const [data,setData] = React.useState(initialData || {});
+  const [data,setData] = React.useState(initialData);
+  
   const editorProps = {
     itemID,
     itemProp,
@@ -21,16 +22,17 @@ const Text = (props) => {
 
   useEffect(() => {
     if(!itemID || !itemProp ) return;
-    if(!initialData) { fetchData(itemID).then((data) => setData(data)) };
-  }, [itemID, itemProp, initialData]);
+    if(!data) { fetchData(itemID).then((data) => setData(data)) };
+  }, [itemID, itemProp, data]);
   
-  return (
+  
+  return data ? (
       itemType !== "richtext" ?(
-          <div {...editorProps} data-editor-itemmodel="text" className={className} data-editor-itemlabel={data.id}>
+          <div {...editorProps} data-editor-itemmodel="text" className={className} data-editor-itemlabel={data?.id}>
             {data[itemProp]}
           </div>
-      ) : <div {...editorProps} data-editor-itemmodel="richtext" className={className}  data-editor-itemlabel={data.id} dangerouslySetInnerHTML={{__html: data[itemProp]}}/>
-  );
+      ) : <div {...editorProps} data-editor-itemmodel="richtext" className={className}  data-editor-itemlabel={data?.id} dangerouslySetInnerHTML={{__html: data[itemProp]}}/>
+  ): <></>;
 };
 
 export default Text;
