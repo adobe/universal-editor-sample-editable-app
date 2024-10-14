a299058440d79ff5b5e123c37f5d18647fb81e08
vlad <ens16477@adobe.com>
2024-10-14 14:02:13 +0300

    fix: add netlify deployment script

---
+++ deploy/script.sh
@@ -0,0 +1,26 @@
+# Prerequisites
+
+# Install Netlify CLI
+# npm install netlify-cli -g
+
+# Set the following environment variables in your terminal settings
+# NETLIFY_AUTH_TOKEN = <authentication token>
+# NETLIFY_SITE_ID = <site id where to deploy>
+
+# Run in project root:
+# <npm run deploy> - If you want to deploy the app to a non-production link, e.g https://62ff59a019923a6f7aec439d--prismatic-panda-c194c0.netlify.app/.
+# <npm run deploy prod> - If you want to deploy the app to the production link https://prismatic-panda-c194c0.netlify.app/.
+
+# If case of permission issues, run <chmod +x deploy/script.sh> at the root of the project.
+
+if npm run build; then
+  echo "Build of E2E app created successfully "
+  netlify link
+  if [ "$1" = "e2e-app" ]; then
+    netlify deploy --prod
+  else
+    netlify deploy
+  fi
+else
+  echo "Build failed, cannot be deployed"
+fi
