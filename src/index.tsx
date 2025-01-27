2cd0ff31140793130dcd3a258d2bce75ccc4d516
Sharanya Vinod <svinod@adobe.com>
2025-01-27 17:10:08 +0100

    fix: adapt to script cors

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "@adobe/universal-editor-cors";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
