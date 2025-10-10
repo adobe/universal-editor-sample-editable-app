7958553ef8b2ce97f8f78d0cef29540ff333b3fe
Jason Wolf <jaydubb12@gmail.com>
2025-10-10 15:16:39 -0400

    Fixes issue #130 that produces confusing runtime issue due to WKND Content Package not containing these paths

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,18 +22,7 @@ function Home() {
         <div className="Home">
             <Teaser />
             <Adventures />
-            <section className="newsletter">
-                <div className="content">
-                    <Title resource="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/title" prop="jcr:title" type="text"/>
-                    <Text resource="urn:aemconnection:/content/wknd/us/en/newsletter/jcr:content/root/container/text" prop="text" type="richtext" />
-                </div>
-                <button>Subscribe</button>
-            </section>
             <section className="about-us">
-                <div className="content">
-                    <Title resource="urn:aemconnection:/content/wknd/language-masters/en/universal-editor-container/jcr:content/root/title" prop="jcr:title" type="text"/>
-                    <Container resource="urn:aemconnection:/content/wknd/language-masters/en/universal-editor-container/jcr:content/root/container" type="container" />
-                </div>
                 <Link to={`/aboutus${window.location.search}`}>
                     <button className="dark">Read more</button>
                 </Link>
