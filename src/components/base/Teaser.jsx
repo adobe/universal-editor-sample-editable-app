5dbfb94a61743733c16f4dbe041c5b2f90bf3ee4
voss <voss@adobe.com>
2026-02-09 17:02:38 +0100

    Tutorial: Teaser Start Point

--- src/components/base/Teaser.jsx
+++ src/components/base/Teaser.jsx
@@ -15,31 +15,12 @@ const Teaser = (props) => {
   const { resource, type, data: initialData } = props;
   const [data, setData] = React.useState(initialData);
 
-  const editorProps = useMemo(() => ({
-    "data-aue-resource": resource,
-    "data-aue-type": type,
-  }), [resource, type]);
-
   useEffect(() => {
     if (!resource) return;
     if (!data) {
       fetchData(resource).then((data) => setData(data));
     }
   }, [resource, data]);
-
-  useEffect(() => {
-    const handleUpdate = (e) => {
-      const { itemids = [] } = e.detail;
-      if (itemids.indexOf(resource) >= 0) {
-        setData(null);
-      }
-      e.stopPropagation();
-    };
-    document.addEventListener("editor-update", handleUpdate);
-    return () => {
-      document.removeEventListener("editor-update", handleUpdate);
-    };
-  }, [resource]);
 
   if (!data) return <></>;
 
@@ -51,15 +32,12 @@ const Teaser = (props) => {
   const ctaLink = data.ctaLink || data["cta:link"] || "";
 
   return (
-    <section {...editorProps} className="teaser" data-aue-component="teaser" data-aue-label="Teaser">
+    <section className="teaser">
       {imagePath && (
         <div className="teaser-image">
           <img
             src={getImageURL(imagePath)}
             alt={title}
-            data-aue-prop="fileReference"
-            data-aue-type="media"
-            data-aue-label="Image"
           />
         </div>
       )}
@@ -67,9 +45,6 @@ const Teaser = (props) => {
         {subtitle && (
           <p
             className="teaser-subtitle"
-            data-aue-prop="subtitle"
-            data-aue-type="text"
-            data-aue-label="Subtitle"
           >
             {subtitle}
           </p>
@@ -77,9 +52,6 @@ const Teaser = (props) => {
         {title && (
           <h2
             className="teaser-title"
-            data-aue-prop="jcr:title"
-            data-aue-type="text"
-            data-aue-label="Title"
           >
             {title}
           </h2>
@@ -87,9 +59,6 @@ const Teaser = (props) => {
         {description && (
           <div
             className="teaser-description"
-            data-aue-prop={"description"}
-            data-aue-type={"richtext"}
-            data-aue-label="Description"
             dangerouslySetInnerHTML={{__html: description}}
           >
           </div>
@@ -98,26 +67,11 @@ const Teaser = (props) => {
           <div className="teaser-cta">
             <a
               href={ctaLink || "#"}
-              className="teaser-cta-button"
-            >
-              <span
-                data-aue-prop="ctaText"
-                data-aue-type="text"
-                data-aue-label="CTA Text"
-              >
+              className="teaser-cta-button">
+              <span>
                 {ctaText}
               </span>
             </a>
-            {ctaLink && (
-              <span
-                data-aue-prop="ctaLink"
-                data-aue-type="text"
-                data-aue-label="CTA Link"
-                style={{ display: 'none' }}
-              >
-                {ctaLink}
-              </span>
-            )}
           </div>
         )}
       </div>
