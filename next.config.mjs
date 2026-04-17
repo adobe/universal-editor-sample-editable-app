956131995c826ad0be397718d5cfdc305df9968c
Alessandro Giupponi <alessandro.giupponi@codeland.it>
2026-04-17 13:58:27 +0200

    Add custom CORS headers for all routes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    debugger;
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://experience.adobe.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS, PUT, PATCH, DELETE" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
+          // CRITICAL: This allows a public site to access your local network
+          { key: "Access-Control-Allow-Private-Network", value: "true" },
+        ],
+      },
+    ]
+  }
 };
 
 export default nextConfig;
